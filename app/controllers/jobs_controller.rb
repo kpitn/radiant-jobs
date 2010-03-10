5fae60986171bbecfb5383d75bef35e6c3e5b5be
pierre <pierre@pierre-desktop.(none)>
2010-03-10 22:17:56 +0100

    update jobs views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < SiteController
 
   def index
     @search = Job.search(params[:search])
-    @jobs = @search.all
+    @jobs = @search.paginate(:per_page => 4, :page => params[:page], :order => "start_published_at DESC")
     radiant_render :page => "/jobs"
   end
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -7,6 +7,6 @@ class Job < ActiveRecord::Base
   validates_uniqueness_of :reference
 
   named_scope :not_deleted, :conditions=> ["deleted_at IS NULL"]
-  named_scope :active, :conditions=> ["start_published_at >= ? and end_published_at <= ? and deleted_at IS NULL",Time.now,Time.now]
+  named_scope :active, :conditions=> ["start_published_at >= ? and end_published_at <= ? and deleted_at IS NULL",Time.now,Time.now], :order => "start_published_at DESC"
 
 end
